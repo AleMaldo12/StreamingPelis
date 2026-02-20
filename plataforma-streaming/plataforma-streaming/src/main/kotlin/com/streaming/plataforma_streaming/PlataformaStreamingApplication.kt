1c3335ef7bc486a86c4194461cb3c27d931c0ef8
AleMaldo12 <pablomaldo990@gmail.com>
2026-02-19 18:57:20 -0600

    creación api

---
+++ plataforma-streaming/plataforma-streaming/src/main/kotlin/com/streaming/plataforma_streaming/PlataformaStreamingApplication.kt
@@ -0,0 +1,11 @@
+package com.streaming.plataforma_streaming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlataformaStreamingApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlataformaStreamingApplication>(*args)
+}
---
+++ plataforma-streaming/plataforma-streaming/src/test/kotlin/com/streaming/plataforma_streaming/PlataformaStreamingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.streaming.plataforma_streaming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlataformaStreamingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
